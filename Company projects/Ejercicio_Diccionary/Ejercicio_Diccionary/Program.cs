826338a05680ffa88f3bc36962145f9231c572db
Mluis01 <luis4471@hotmail.com>
2017-03-31 12:23:12 -0500

    Company projects
    This is a demonstration of my learning in hsbcad

---
+++ Company projects/Ejercicio_Diccionary/Ejercicio_Diccionary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Diccionary
+{
+    public class Client
+    {
+        private string m_strName = "";
+
+        public string Name
+        {
+            get { return m_strName; }
+        }
+
+        public Client(string strName)
+        {
+            m_strName = strName;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> lstClients = new List<Client>();
+
+            Client client = new Client("Marcelo");
+            lstClients.Add(client);
+
+            Client client2 = new Client("Luis");
+            lstClients.Add(client2);
+
+            Client client3 = new Client("Juan");
+            lstClients.Add(client3);
+
+            Console.WriteLine();
+            foreach (Client clientAux in lstClients)
+            {
+                Console.WriteLine(clientAux.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
